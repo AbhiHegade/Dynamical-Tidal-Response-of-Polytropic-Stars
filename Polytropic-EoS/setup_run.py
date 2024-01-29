2a3b6d2c055f4bb759d074ff55520165e2a6c79b
AbhiHegade <abhikumbale@gmail.com>
2024-01-29 16:49:53 +0530

    forgot to subtract log, corrected

--- Polytropic-EoS/setup_run.py
+++ Polytropic-EoS/setup_run.py
@@ -32,7 +32,7 @@ def get_input_array_from_b(n,N1,barr):
     input_data = np.array(input_data)
     return input_data
 #===============================================================================
-input_data = get_input_array_from_b(1.0,0.75, [0.0054320988, 0.1358024691, 
+input_data = get_input_array_from_b(1.0,1.0, [0.0054320988, 0.1358024691, 
 0.2661728395,
 0.347654321 ])
 #===============================================================================
@@ -47,10 +47,10 @@ sim.N_write = 99
 sim.steps_num = 5
 sim.factor_match = 0.7
 #----------------------
-sim.Omegalow = 0.06
-sim.Omegahigh = 0.16
+sim.Omegalow = 0.01
+sim.Omegahigh = 0.5
 sim.div_Omega = 1000
-sim.out_dir = out_path + "/Runs"
+sim.out_dir = out_path + "/Runs/n_1_N_1"
 sim.animscript = home_path + "/plot-vals.ipynb"
 sim.setup_script = home_path + "/setup_run.py"
 #----------------------
